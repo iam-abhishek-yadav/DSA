3ae5632b0afe49569998475ff7b9d4c61a4c002b
iam-abhishek-yadav <abhrecscs17@gmail.com>
2025-11-11 08:44:43 +0000

    Add Leetcode all

---
+++ Leetcode/All/0002_add_two_number/solution.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    carry := 0
+    dummyHead := &ListNode{}
+    current := dummyHead
+
+    for l1 != nil || l2 != nil || carry != 0 {
+        val1, val2 := 0, 0
+        if l1 != nil {
+            val1 = l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            val2 = l2.Val
+            l2 = l2.Next
+        }
+
+        sum := val1 + val2 + carry
+        carry = sum / 10
+
+        current.Next = &ListNode{Val: sum % 10}
+        current = current.Next
+    }
+
+    return dummyHead.Next
+}
