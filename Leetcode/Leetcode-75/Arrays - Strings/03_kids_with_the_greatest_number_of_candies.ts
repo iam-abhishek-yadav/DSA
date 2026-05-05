b926c5efb3b75e602e2c6187d029fcff787ca178
iam-abhishek-yadav <abhrecscs17@gmail.com>
2026-05-05 19:48:12 +0530

    Clear out old commit

--- Leetcode/Leetcode-75/Arrays - Strings/03_kids_with_the_greatest_number_of_candies.ts
+++
@@ -1,9 +0,0 @@
-function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    const max = Math.max(...candies);
-    const res = [];
-    for(const candy of candies){
-        if(candy + extraCandies >= max) res.push(true)
-        else res.push(false)
-    }
-    return res
-};  
